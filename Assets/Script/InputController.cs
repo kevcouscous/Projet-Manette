0e7be57fad34e5f8958a58ab0992423282a01b1c
@coussemacker <kevin.coussemacker@telecom-sudparis.eu>
2017-10-08 16:37:57 +0200

    Initial commit

---
+++ Assets/Script/InputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    private float horizontal = 0;
+    private float vertical = 0;
+    public float multHorizontal = 0;
+    public float multVertical = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(horizontal * multHorizontal, vertical * multVertical, 0);
+    }
+}
